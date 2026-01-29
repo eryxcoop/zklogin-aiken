53c7ed58a69b99468d66dc2929bc918a00822831
sfedi-eryx <sfedi@eryx.co>
2026-01-29 16:42:13 -0300

    Improved test to hash (sha256) a header+payload and compare it to its hash.

--- backend/circuit_tests/zkLogin.test.js
+++ backend/circuit_tests/zkLogin.test.js
@@ -12,7 +12,7 @@ describe("Poseidon Circuit test", function () {
 
     beforeAll( async () => {
         circuit = await wasm_tester(path.join(__dirname, "prueba.circom"));
-    });
+    }, 1000000);
 
     it("XXX", async () => {
         const witness = await circuit.calculateWitness({inputs: []}, true);
@@ -23,7 +23,7 @@ describe("Poseidon Circuit test", function () {
         // eyJhbGciOiJSUzI1NiIsImtpZCI6Ijk1NDRkMGZmMDU5MGYwMjUzMDE2NDNmMzI3NWJmNjg3NzY3NjU4MjIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI5MjA3MTkyMzA0MjEtbzZrdDUyMzVjczhwbTBic2ZkdGt0MGk1ZmxhN20wNzIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI5MjA3MTkyMzA0MjEtbzZrdDUyMzVjczhwbTBic2ZkdGt0MGk1ZmxhN20wNzIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDI1NjMxNzQzNTY1ODczMDA2MTgiLCJub25jZSI6ImIvaTFmVUtVTEhpMzB0R25yZU80Y3BJQ0FoOXdmUFVkdUJ2bWNvYmpMWk09IiwibmJmIjoxNzY5NjI3NTA4LCJpYXQiOjE3Njk2Mjc4MDgsImV4cCI6MTc2OTYzMTQwOCwianRpIjoiZTQ4NGQxZTFhYjMxYmJmYzgwZGVlZjQyYzg1YzI0NTE3MmI5NTI0YiJ9
 
         //assert(F.eq(F.e("1018317224307729531995786483840663576608797660851238720571059489595066344487"), F.e(res2)));
-        //await circuit.assertOut(w, {out : F.toObject(res2)});
+        // await circuit.assertOut(witness, {hash : F.toObject(res2)});
         await circuit.checkConstraints(witness);
     });
 });
