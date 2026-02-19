1168dd238ed1294d766bc65ec57045d92c802c52
sfedi-eryx <sfedi@eryx.co>
2026-02-19 18:10:26 -0300

    Removed `jest.config.js` and updated Jest dependencies. Upgraded `package.json` and `package-lock.json` to use newer versions of dependencies. Simplified testing configuration. Add xxx.test.ts proof of concept.

---
+++ backend/deployment/xxx.test.ts
@@ -0,0 +1,9 @@
+import {derivateAddress} from './addressDerivation'
+
+describe("Circuit test", function () {
+
+    it("verifies jwt signature using RS256", async () => {
+
+    }, 1000000);
+
+});
