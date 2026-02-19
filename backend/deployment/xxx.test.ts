1cac48931300ac58c1cd50adf658cba04c81c48f
sfedi-eryx <sfedi@eryx.co>
2026-02-19 18:40:11 -0300

    Added a bit of logic to the prototype test xxx.test.ts

--- backend/deployment/xxx.test.ts
+++ backend/deployment/xxx.test.ts
@@ -4,7 +4,7 @@ import {derivateAddress} from './addressDerivation.ts'
 describe("Circuit test", function () {
 
     it("verifies jwt signature using RS256", async () => {
-
+        derivateAddress(BigInt("21140065873708661981141523561235886173184262775540053915030560953831497869414"));
     }, 1000000);
 
 });
