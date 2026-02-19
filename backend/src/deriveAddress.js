92ee581463794384a6bfb563e0d5c2a6633cfbf7
bruno <bweisz@eryxsoluciones.com.ar>
2026-02-19 16:09:01 -0300

    Extracted handler to different file

---
+++ backend/src/deriveAddress.js
@@ -0,0 +1,26 @@
+import {derivateAddress} from "../deployment/addressDerivation.js";
+
+export function handleDeriveAddressAction(searchParams, res) {
+    try {
+        const zkLoginId = searchParams.get("zkLoginId");
+        const derivedAddress = derivateAddress(BigInt(zkLoginId));
+        console.log("Execution of script succeeded");
+        const responseObject = {
+            message: 'Wallet created successfully.',
+            execution_result_code: 0,
+            status: 'success',
+            walletAddress: derivedAddress
+        };
+    } catch (error) {
+        console.log("Execution of script failed");
+        const responseObject = {
+            message: error.message,
+            execution_result_code: error,
+            status: 'error',
+            walletAddress: '----'
+        };
+    }
+    const statusCode = responseObject.status === "error" ? 422 : 200; // 422 Unprocessable Content. The request was well-formed (i.e., syntactically correct) but could not be processed.
+    res.writeHead(statusCode, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(responseObject));
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 // Import the built-in http module
 import http from "http";
-import {ZKLOGIN_ID} from "../deployment/transactionData.ts";
-import {derivateAddress} from "../deployment/addressDerivation.ts";
+import {handleDeriveAddressAction} from "./deriveAddress.js";
 
 const DERIVE_ADDRESS_PATHNAME = "/deriveAddress"
+const GENERATE_PROOF_PATHNAME = "/deriveAddress"
 
 const host = 'localhost';
 const port = 8000;
@@ -23,40 +23,15 @@ const requestListener = function (req, res) {
         return;
     }
 
-    let responseObject = {
-        message: 'Something bad happened',
-        status: 'error',
-        walletAddress: ''
-    };
-
     const url = new URL(req.url, `http://${host}:${port}`);
     const method = req.method;
     const searchParams = url.searchParams;
     const pathname = url.pathname;
 
     if (method === "GET" && pathname === DERIVE_ADDRESS_PATHNAME) {
-        try {
-            const zkLoginId = searchParams.get("zkLoginId");
-            const derivedAddress = derivateAddress(BigInt(zkLoginId));
-            console.log("Execution of script succeeded");
-            responseObject = {
-                message: 'Wallet created successfully.',
-                execution_result_code: 0,
-                status: 'success',
-                walletAddress: derivedAddress
-            };
-        } catch(error) {
-            console.log("Execution of script failed");
-            responseObject = {
-                message: error.message,
-                execution_result_code: error,
-                status: 'error',
-                walletAddress: '----'
-            };
-        }
-        const statusCode = responseObject.status === "error" ? 422 : 200; // 422 Unprocessable Content. The request was well-formed (i.e., syntactically correct) but could not be processed.
-        res.writeHead(statusCode, {"Content-Type": "application/json"});
-        res.end(JSON.stringify(responseObject));
+        handleDeriveAddressAction(searchParams, res);
+    } else if (method === "POST" && pathname === GENERATE_PROOF_PATHNAME) {
+
     } else {
         throw Error("Unknown combination of request method and path name")
     }
@@ -69,3 +44,4 @@ const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+
