8ac691268fa58789d2231f825320fc6834fb45df
sfedi-eryx <sfedi@eryx.co>
2026-02-20 17:31:31 -0300

    Improved error handling when plutus.json is missing.

--- backend/deployment/deriveAddress.ts
+++ backend/deployment/deriveAddress.ts
@@ -6,8 +6,16 @@ import {ZKLOGIN_ID} from "./transactionData.ts"
 export function deriveAddress(zkloginid: bigint) {
     const compiledContractPath = "./plutus.json";
     const validatorScriptIndex = 0;
+    let jsonString: string;
 
-    const jsonString = readFileSync(compiledContractPath, 'utf-8');
+    try {
+        jsonString = readFileSync(compiledContractPath, 'utf-8');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error("plutus.json does not exist, run 'aiken build' as stated in README.md");
+        }
+        throw error;
+    }
     const parsedJson = JSON.parse(jsonString);
 
     const parameterized_script = applyParamsToScript(
