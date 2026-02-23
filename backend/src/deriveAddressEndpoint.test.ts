3468e72167cf30ffafeb3a6a44e687b217b5e855
sfedi-eryx <sfedi@eryx.co>
2026-02-23 11:11:23 -0300

    Groomed code.

--- backend/src/deriveAddressEndpoint.test.ts
+++ backend/src/deriveAddressEndpoint.test.ts
@@ -12,16 +12,7 @@ describe("Derive address endpoint tests", () => {
         const url = new URL(request.url, `http://${host}:${port}`);
         const searchParams = url.searchParams;
 
-        function Response() {
-            this.status = undefined;
-            this.writeHead = (statusCode, headers: any) => {
-                this.status = statusCode;
-            };
-            this.end = (something: string) => {
-            };
-        };
-        let response = new Response();
-        return handleDeriveAddressAction(searchParams, response);
+        return handleDeriveAddressAction(searchParams);
     }
 
     function assertResponseIs200(response: any) {
@@ -32,12 +23,11 @@ describe("Derive address endpoint tests", () => {
         assert.equal(response.body['walletAddress'], 'addr_test1wz85pmuldpc0km0rcdjcu8c8a2c0rvcklut6puhhq0wu9ygvfat33');
     }
 
-    it("xxx", () => {
+    it("answers successfully when correct parameters are passed", () => {
         const request = {
             url: '/deriveAddress?zkLoginId=21140065873708661981141523561235886173184262775540053915030560953831497869414',
         };
 
-        // Call endpoint
         const actualResponse = callEndpoint(request);
 
         assertResponseIs200(actualResponse)
--- backend/src/handleDeriveAddressAction.ts
+++ backend/src/handleDeriveAddressAction.ts
@@ -1,6 +1,6 @@
 import {deriveAddress} from "../deployment/deriveAddress.ts";
 
-export function handleDeriveAddressAction(searchParams, res) {
+export function handleDeriveAddressAction(searchParams) {
     let responseObject;
     try {
         const zkLoginId = searchParams.get("zkLoginId");
