60de35e411696da938e133b3a1e7fff064b30407
sfedi-eryx <sfedi@eryx.co>
2026-02-25 10:01:17 -0300

    Clarified comments in `generateProof` function to improve code readability.

--- backend/deployment/generateProof.ts
+++ backend/deployment/generateProof.ts
@@ -3,13 +3,15 @@ import * as path from 'node:path';
 
 export async function generateProof(inputZkLoginData: Record<string, unknown>, proofFile) {
 
+    // Determine path where to create file with circuit parameters
     // Create file with json data
     const inputZkLoginFileContent = JSON.stringify(inputZkLoginData, null, 2);
 
     // Invoke external tool to create proof
+    // const toolGeneratedProofFile = ???
 
     // Move proof file to expected location
-    //await fs.rename(proofFile, proofFile);
+    //await fs.rename(toolGeneratedProofFile, proofFile);
     const proofDir = path.dirname(proofFile);
     await fs.mkdir(proofDir, {recursive: true});
     await fs.writeFile(proofFile, 'dummy content', 'utf8');
