b55e666026b0297c862bd9e1676aba205ddd5d65
sfedi-eryx <sfedi@eryx.co>
2026-03-03 11:45:52 -0300

    Abstracted proof generation logic into a parameterized action, added support for fast and slow test configurations, and updated related run configurations and test files.

--- backend/src/handleGenerateProofEndpoint.ts
+++ backend/src/handleGenerateProofEndpoint.ts
@@ -2,12 +2,15 @@ import {generateProof} from "../deployment/generateProof.ts";
 import {execSync} from "node:child_process";
 import * as fs from 'node:fs/promises';
 
-export async function handleGenerateProofEndpoint(request) {
+export async function handleGenerateProofEndpoint(request, generateProofAction = null) {
     try {
         let proofFilePath = 'generated_proofs/zk_redeemer.ts';
-        const generateProofAction = (toolExecutionCommandLine) => {
-            execSync(toolExecutionCommandLine, {encoding: 'utf8'});
-        };
+        if (generateProofAction === null) {
+            generateProofAction = (toolExecutionCommandLine) => {
+                execSync(toolExecutionCommandLine, {encoding: 'utf8'});
+            };
+        }
+
         const proofPath = await generateProof(
             generateProofAction,
             request.body,
