2897bfac6a193d7d087905c2a87180d63fb45f2f
sfedi-eryx <sfedi@eryx.co>
2026-03-05 16:26:11 -0300

    Add error-handling test for `handleFundAddressEndpoint` with unsuccessful execution logic

--- backend/src/fundAddressEndpoint.test.ts
+++ backend/src/fundAddressEndpoint.test.ts
@@ -8,12 +8,20 @@ describe("Fund address endpoint tests", () => {
         assert.equal(response.status, 200);
     }
 
+    function assertResponseIs500(response: any) {
+        assert.equal(response.status, 500);
+    }
+
     function assertResponseHasField(fieldName: string, actualResponse: any) {
         assert.ok(Object.hasOwn(actualResponse.body, fieldName))
     }
 
     function endpointLogicThatExecutesSuccessfully(_scriptAddr) {
         return 'a valid transaction hash';
+    }
+
+    function endpointLogicThatExecutesUnsuccessfully(_scriptAddr) {
+        throw new Error('Forced error');
     }
 
     it("answers successfully when correct parameters are passed", async () => {
@@ -29,5 +37,20 @@ describe("Fund address endpoint tests", () => {
         assertResponseIs200(actualResponse);
         assertResponseHasField('message', actualResponse);
         assertResponseHasField('transactionHash', actualResponse);
+    });
+
+    it("answers with error when a problem occurs", async () => {
+        const request = {
+            url: '/fundAddress',
+            body: {
+                'zkLoginAddress': '',
+            },
+        };
+
+        const actualResponse = await handleFundAddressEndpoint(request, endpointLogicThatExecutesUnsuccessfully);
+
+        assertResponseIs500(actualResponse);
+        assertResponseHasField('message', actualResponse);
+        assertResponseHasField('error', actualResponse);
     });
 });
