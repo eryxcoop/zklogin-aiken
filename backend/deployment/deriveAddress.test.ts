5ad4bf6501ba811d495b884f7560d7aaca5b9d66
sfedi-eryx <sfedi@eryx.co>
2026-03-06 17:00:35 -0300

    Refactor `deriveAddress.test.ts`: destructure returned object in test implementation

--- backend/deployment/deriveAddress.test.ts
+++ backend/deployment/deriveAddress.test.ts
@@ -5,7 +5,7 @@ import * as assert from 'node:assert/strict';
 describe("Derive address tests", function () {
 
     it("happy path", async () => {
-        const derivedAddress = await deriveAddress(BigInt('21140065873708661981141523561235886173184262775540053915030560953831497869414'));
+        const {derivedAddress} = await deriveAddress(BigInt('21140065873708661981141523561235886173184262775540053915030560953831497869414'));
 
         const addressBeginning = derivedAddress.slice(0, 5);
         const expectedBeginning = 'addr_';
